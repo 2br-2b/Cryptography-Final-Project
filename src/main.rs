74d84282b982252178643767fcc4ff6d1e31ee88
2br-2b <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2021-12-01 10:06:48 -0500

    Removed unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,8 @@
-//https://stackoverflow.com/questions/35833329/how-to-convert-i32-to-a-string-representing-the-ascii-character
-
 use std::io;
 use rand::Rng;
 use primes;
-use std::cmp::Ordering;
 use modinverse::modinverse;
 use num::BigInt;
-
-
-fn main() {
-    //println!("Welcome! Would you like to:\n    1. Generate a new prime number;");
-/*    
-    let mut choice = String::new();
-
-    loop{
-        println!("Welcome! Would you like to:\n    1. Generate a new prime number;\n2. Encrypt a message;\n3. Decrypt a message.\n");
-        
-        let mut choice = String::new();
-        
-        io::stdin().read_line(&mut choice).expect("Failed to read line");        
-
-        let choice: u8 = match choice.trim().parse(){
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number!\n");
-                continue;
-            },
-        };
-
-        let numb: u8 = 3;
-
-        match choice.cmp(&numb){
-            Ordering::Less | Ordering::Equal => break,
-            Ordering::Greater => {
-                println!("Please enter a valid number!\n");
-                continue;
-            },
-        }
-        
-    }
-    
-    let a="1".to_string();
-    let b="2".to_string();
-    let c="3".to_string();
-
-
-    match choice {
-
-        a => generate_prime_user(),
-        b => encrypt_message(),
-        c => decrypt_message(),
-        _ => println!("Error!"),
-
-
-    }
-
-*/
-    encrypt_message();
-}
-
 
 fn generate_prime() -> i128 {
     loop{
@@ -74,12 +18,8 @@ fn generate_prime() -> i128 {
     }
 }
 
-fn generate_prime_user(){
-    println!("Generating a new prime number...");
-    println!("{} is a prime number!", generate_prime());
-}
 
-fn encrypt_message(){
+fn main(){
 
     let p : i128= generate_prime();
     let q : i128 = generate_prime();
@@ -90,7 +30,6 @@ fn encrypt_message(){
 
     let e: i128 = 2_i128.pow(16) + 1;
 
-    //let mut d:i128 = modinverse(e, phi);
     let mut d:i128 = 0;
 
     let does_exist = modinverse(e, phi);
@@ -120,27 +59,13 @@ fn encrypt_message(){
 
 }
 
-fn decrypt_message(){
-    println!("Input c:");
-    let c:i128 = input_int();
-    println!("Input d:");
-    let d:i128 = input_int();
-    println!("Input n:");
-    let n:i128 = input_int();
-
-    let decrypted:u64 = mod_pow(c.into(), d, n).into();
-    println!("{} ^ {} mod {} = {}", c, d, n, decrypted);
-
-}
-
-
-fn mod_pow(mut base: i128, mut exp: i128, modulus: i128) -> u64 {
+fn mod_pow(base: i128, exp: i128, modulus: i128) -> u64 {
    
-    //Warning: Converting from BigInts to u64, so there may be some slight data loss
+    // **Warning**: Converting from BigInts to u64, so there may be some slight data loss
 
     let result = BigInt::modpow(&BigInt::from(base), &BigInt::from(exp), &BigInt::from(modulus));
     
-    let (a, b) = result.to_u64_digits();
+    let (_a, b) = result.to_u64_digits();
     
     b[0]
 }
